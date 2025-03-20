a053b0b9706b33feccd4bf70931f3e3e834ad868
ApacheTech Solutions <p_matthews@live.co.uk>
2025-03-20 22:14:26 +0000

    repo: update github templates

--- src/AccessibilityTweaks/Features/SoundEffects/Dialogue/SoundEffectsDialogue.cs
+++ src/AccessibilityTweaks/Features/SoundEffects/Dialogue/SoundEffectsDialogue.cs
@@ -100,7 +100,7 @@ public sealed class SoundEffectsDialogue : FeatureSettingsDialogue<SoundEffectsS
             }
             catch (Exception exception)
             {
-                ApiEx.Logger.Error(exception);
+                G.Logger.Error(exception);
                 return [];
             }
         }
--- src/AccessibilityTweaks/Program.cs
+++ src/AccessibilityTweaks/Program.cs
@@ -1,3 +1,30 @@
-﻿namespace ApacheTech.VintageMods.AccessibilityTweaks;
+﻿
+using ApacheTech.Common.BrighterSlim;
+using Gantry.Core.Annotation;
+using Gantry.Services.Brighter.Filters;
+using Gantry.Services.BrighterChat;
 
-public sealed class Program : ModHost;
+namespace ApacheTech.VintageMods.AccessibilityTweaks;
+
+public sealed class Program : ModHost
+{
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        base.StartClientSide(api);
+        api.ChatCommands
+            .Create("brighter")
+            .WithMediatedHandler<BrighterCommand>();
+    }
+}
+
+public class BrighterCommand : MediatedChatCommand;
+
+public class BrighterCommandHandler : RequestHandler<BrighterCommand>
+{
+    [HandledOnClient]
+    public override BrighterCommand Handle(BrighterCommand command)
+    {
+        command.Result = TextCommandResult.Success("Yayyyyyyy!");
+        return base.Handle(command);
+    }
+}
