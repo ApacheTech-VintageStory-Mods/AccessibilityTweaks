04d97695a27d7dfc3b7e5882c8965fae5c9311cc
ApacheTech Solutions <p_matthews@live.co.uk>
2025-09-18 22:48:50 +0100

    feat: overhaul AccessibilityTweaks mod structure
    - Replaced original `Program` class with a new implementation inheriting from `G.Host<Program>`.
    - Removed `BrighterCommand` and its handler, indicating a shift in functionality.
    - Updated mod version from "3.8.4" to "4.0.0" and game dependency from "1.20.6" to "1.21.0".
    - Cleaned up global using directives, focusing on `AccessibilityTweaks` features.

--- src/AccessibilityTweaks/Program.cs
+++ src/AccessibilityTweaks/Program.cs
@@ -1,30 +1,3 @@
-﻿
-using ApacheTech.Common.BrighterSlim;
-using Gantry.Core.Annotation;
-using Gantry.Services.Brighter.Filters;
-using Gantry.Services.BrighterChat;
+﻿namespace AccessibilityTweaks;
 
-namespace ApacheTech.VintageMods.AccessibilityTweaks;
-
-public sealed class Program : ModHost
-{
-    public override void StartClientSide(ICoreClientAPI api)
-    {
-        base.StartClientSide(api);
-        api.ChatCommands
-            .Create("brighter")
-            .WithMediatedHandler<BrighterCommand>();
-    }
-}
-
-public class BrighterCommand : MediatedChatCommand;
-
-public class BrighterCommandHandler : RequestHandler<BrighterCommand>
-{
-    [HandledOnClient]
-    public override BrighterCommand Handle(BrighterCommand command)
-    {
-        command.Result = TextCommandResult.Success("Yayyyyyyy!");
-        return base.Handle(command);
-    }
-}
+internal sealed class Program : G.Host<Program>;
